60c44ca5c8f00bfcad9d9d982fc0c16e2a4449dc
hungduong.mess32@gmail.com <minhle10072000>
2021-09-30 00:49:56 +0700

    First Commit

---
+++ BoxEnglish/src/main/java/com/hdn/controller/Test.java
@@ -0,0 +1,16 @@
+package com.hdn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Test {
+	
+	@GetMapping
+	public String Default() {
+		return "index";
+	}
+	
+}
