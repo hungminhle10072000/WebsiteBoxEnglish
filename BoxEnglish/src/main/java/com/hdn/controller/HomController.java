ca05c6a1a6e330b5fa813ea0d60aaabc799f913e
nghiabv120100 <nghiabv120100@gmail.com>
2021-12-03 23:05:09 +0700

    fix conplict

--- BoxEnglish/src/main/java/com/hdn/controller/HomController.java
+++ BoxEnglish/src/main/java/com/hdn/controller/HomController.java
@@ -15,14 +15,7 @@ import com.hdn.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import com.hdn.entity.CategoryEntity;
 import com.hdn.entity.UserEntity;
